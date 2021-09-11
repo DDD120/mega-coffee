83f3ca0cc921bd80252938dec98abd5ebe1f0a7f
DDD120 <dltnals9794@naver.com>
2021-09-11 19:05:54 +0900

    Modify responsive

--- src/js/responsive.js
+++ src/js/responsive.js
@@ -1,30 +1,24 @@
-const mediaQuery = window.matchMedia("screen and (max-width: 768px)");
+// TIRGGER
+const triggerEl = document.querySelector("header .trigger");
+const mainMenuEl = document.querySelector("header .main-menu");
 
-mediaQuery.addListener(function (e) {
-  if (e.matches) {
-    // TIRGGER
-    const triggerEl = document.querySelector("header .trigger");
-    const mainMenuEl = document.querySelector("header .main-menu");
-
-    triggerEl.addEventListener("click", () => {
-      if (triggerEl.classList.contains("active")) {
-        mainMenuEl.classList.remove("active");
-        triggerEl.classList.remove("active");
-      } else {
-        mainMenuEl.classList.add("active");
-        triggerEl.classList.add("active");
-      }
-    });
-
-    // SHOW MENU CONTENTS
-    const nameEls = document.querySelectorAll("header .name");
-
-    for (nameEl of nameEls) {
-      nameEl.addEventListener("click", (event) => {
-        event.target.classList.contains("show")
-          ? event.target.classList.remove("show")
-          : event.target.classList.add("show");
-      });
-    }
+triggerEl.addEventListener("click", () => {
+  if (triggerEl.classList.contains("active")) {
+    mainMenuEl.classList.remove("active");
+    triggerEl.classList.remove("active");
+  } else {
+    mainMenuEl.classList.add("active");
+    triggerEl.classList.add("active");
   }
 });
+
+// SHOW MENU CONTENTS
+const nameEls = document.querySelectorAll("header .name");
+
+for (nameEl of nameEls) {
+  nameEl.addEventListener("click", (event) => {
+    event.target.classList.contains("show")
+      ? event.target.classList.remove("show")
+      : event.target.classList.add("show");
+  });
+}
